5772abf5d2e779787a691ea3820b957415f21840
Lance Stephens <4097471+pythoninthegrass@users.noreply.github.com>
2023-02-13 16:57:24 -0600

    Check available ports during startup
    Test via `nc -k -l $PORT`

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# shellcheck disable=SC1091,SC2034,SC2153,SC2236,SC3037,SC3045,SC2046
+# shellcheck disable=SC1091,SC2034,SC2086,SC2153,SC2236,SC3037,SC3045,SC2046
 
 if [ "$(uname -s)" = "Darwin" ]; then
 	export VENV=".venv"
@@ -11,10 +11,26 @@ export PATH="${VENV}/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:$PATH"
 
 # . "${VENV}/bin/activate"
 
-main() {
-	if [ ! -z "$1" ]; then
+# TODO: cleanup (case statement?)
+port_check() {
+	if [ ! -z "$1" ] && [ -z "$(lsof -i :$1 | grep LISTEN | awk '{print $2}')" ]; then
 		PORT="$1"
+	elif [ ! -z "$1" ] && [ ! -z "$(lsof -i :$1 | grep LISTEN | awk '{print $2}')" ]; then
+		PORT=$(($1+1))
+		echo "Port $1 is in use, trying $PORT"
+		while [ ! -z "$(lsof -i :$PORT | grep LISTEN | awk '{print $2}')" ]; do
+			echo "Port $PORT is in use, trying $((PORT+1))"
+			PORT=$((PORT+1))
+		done
+		echo "Port $PORT is available. Using it instead of $1"
+	else
+		echo "Failed to find available port. $1 is in use. Exiting... "
+		exit 1
 	fi
+}
+
+main() {
+	port_check "$@"
 	gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app -b "0.0.0.0:${PORT:-3000}" --log-file -
 }
 main "$@"
